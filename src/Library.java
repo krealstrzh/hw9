499a64a250b0da7d289721294403c622cbb4a369
kirillstrizenko <krealstrzh@icloud.com>
2022-08-05 16:37:43 +0300

    HomeWork 9

--- src/Library.java
+++ src/Library.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class Library {
     private final Book[] books;
 
@@ -19,34 +21,48 @@ public class Library {
             if (books[i] != null) {
                 System.out.println(books[i].getAuthor() + ": " + books[i].getBookName() + ": " + books[i].getPublishYear());
             } else {
-                break;
+                System.out.println("Пустой лот!");
+                continue;
             }
         }
     }
 
     public void printBookInfo(String bookName) {
+        int index = -1;
         for (int i = 0; i < books.length; i++) {
             if (books[i] != null) {
                 if (books[i].getBookName().equalsIgnoreCase(bookName)) {
-                    System.out.println(books[i].getBookName() + " by " + books[i].getAuthor() + " was published in " + books[i].getPublishYear());
-                } else {
-                    System.out.println("Книга не найдена :(");
+//                    System.out.println(books[i].getBookName() + " by " + books[i].getAuthor() + " was published in " + books[i].getPublishYear());
+                    index = i;
+                    break;
                 }
             } else {
                 break;
             }
         }
+        if (index > -1) {
+            System.out.println(books[index].getBookName() + " by " + books[index].getAuthor() + " was published in " + books[index].getPublishYear());
+        } else {
+            System.out.println("Книга не найдена :(");
+        }
     }
 
     public void changePubYear(String bookTitle, int newYear){
+        int index = -1;
             for (int i = 0; i < books.length; i++) {
                 if (books[i] != null) {
                     if (books[i].getBookName().equalsIgnoreCase(bookTitle)) {
-                        books[i].setPublishYear(newYear);
+                        index = i;
                     }
                 } else {
                     break;
                 }
             }
+        if (index > -1) {
+            books[index].setPublishYear(newYear);
+            System.out.println("Год успешно изменен!");
+        } else {
+            System.out.println("Книга не найдена!");
+        }
         }
 }
